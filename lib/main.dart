428f77142dfe628457060663f6e5d064f57b497c
@PauloVitor_OFonseca <paulo@geekbusinesslab.com.br>
2021-10-09 20:10:43 -0300

    Inicializando firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:ecommerce/app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    GetMaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return GetMaterialApp(
+            theme: ThemeData(primarySwatch: Colors.purple),
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+          );
+        }
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
 }
