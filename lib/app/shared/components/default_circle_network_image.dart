e9aa33ffe220e0988f69fd46b56899d66ddefd57
@PauloVitor_OFonseca <paulo@geekbusinesslab.com.br>
2021-10-09 21:32:22 -0300

    Remoção de código inútil

--- lib/app/shared/components/default_circle_network_image.dart
+++ lib/app/shared/components/default_circle_network_image.dart
@@ -1,7 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecommerce/app/shared/components/default_circular_progress_indicator.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DefaultCircleNetworkImage extends StatelessWidget {
   final String photo;
